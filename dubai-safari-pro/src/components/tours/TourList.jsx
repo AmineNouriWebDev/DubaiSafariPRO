582334f6bb4460e36bd9d915d3d7f3add1122c80
AmineNouriWebDev <macbookair@MacBook-Air-de-macbookair.local>
2024-06-21 20:07:32 +0100

    adding tour item & styling

--- dubai-safari-pro/src/components/tours/TourList.jsx
+++ dubai-safari-pro/src/components/tours/TourList.jsx
@@ -1,27 +1,12 @@
 import "./tour-list.css";
+import TourItem from "./TourItem";
 const TourList = ({toursList}) =>{
     return (
     <div className="tour-list">
-        {toursList.map(item => 
-        <div className="tour-item">
-            <img src={item.image} alt={item.title} className="tour-item-img" />
-            <span className="tour-item-label">ADVENTURE</span>
-            <div className="tour-item-body">
-            <div className="tour-item-title">{item.title}</div>
-            <div className="tour-item-duration">
-                <span>{item.duration}</span> <i className="bi bi-dot"></i>
-                Pickup available
-            </div>
-            <div>Rating</div>
-            <div className="tour-item-price">
-                <strong>From ${item.priceFrom} </strong>per person
-            </div>
-            <button className="tour-item-link">
-                See More
-            </button>
-            </div>
-        </div>)}
+        {toursList.map((item) => (
+            <TourItem item={item} key = {item.id}/>
+        ))}
     </div>
 );
-}
+};
 export default TourList;
