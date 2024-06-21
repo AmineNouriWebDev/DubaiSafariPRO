b0f26e8d8bf7859a9b240f1f95de58b7acca6bc3
Mohamed Amine Nouri <mohamedamine.nouri@uhd.local>
2024-06-21 15:03:00 +0100

    add Hero Header

--- dubai-safari-pro/src/App.js
+++ dubai-safari-pro/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/header/Header';
+import HeroHeader from './components/hero-header/HeroHeader';
 function App() {
   return (
     <div className="App">
       <Header />
+      <HeroHeader />
     </div>
   );
 }
