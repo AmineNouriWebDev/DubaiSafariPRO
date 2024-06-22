840761a52708be8dcac96d5a7e0abe815d1f7dac
AmineNouriWebDev <macbookair@MacBook-Air-de-macbookair.local>
2024-06-22 16:17:36 +0100

    styling about page

--- dubai-safari-pro/src/pages/about/About.jsx
+++ dubai-safari-pro/src/pages/about/About.jsx
@@ -1,4 +1,17 @@
+import "./about.css";
 const About = ()=>{
-    return (<h1>About Page</h1>);
+    return (
+        <div className="about">
+            <h1>About This App</h1>
+            <p>
+                This app is all about booking safari desert tour in dubai
+            </p>
+            <p>
+                <strong>
+                    version: 1.0.0
+                </strong>
+            </p>
+        </div>
+    );
 }
 export default About;
